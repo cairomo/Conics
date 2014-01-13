b95cf631a4ef536eefd804e399b5c60d4f853287
Chloe Mo <chloe.mo@gmail.com>
2014-01-13 10:52:45 -0800

    Update Conics.java
    took away some methods, added some others

--- Conics.java
+++ Conics.java
@@ -11,13 +11,9 @@ public class Conics {
     int D = 0; //x
     int E = 0; //y
     int F = 0; //constant
-    static String x2 = "x^2";
-    static String xy = "xy";
-    static String y2 = "y^2";
-    static String x = "x";
-    static String y = "y";
-    String[] terms = new String[]{x2,xy,y2,x,y};
-    //probably a lot of unnecessary variables?
+    static int[] indices = new int[5];
+    static int[] coefficients = new int[5];
+
     
     public static boolean isCoeff(String equation, String term){
     	//if the equation has given term
@@ -26,18 +22,29 @@ public class Conics {
     	}
     	return true;
     }
-	
-    public static int findCoeff(String equation, String term){
-		//finds x^2 coefficient and returns it as an integer
-    	int index, coeff, prev;
-    	index = equation.indexOf(term);
-    	prev = 
-    	coeff = Integer.parseInt(equation.substring(0,index)); //substring should not be (0,index) because that works for only the x^2 coefficient
-    	return coeff;
+    
+    public static boolean isXY(String equation){
+    	//checks to see if the xy term exists (it should not)
+    	if(equation.indexOf("xy") != -1){
+    		return false;
+    	}
+    	else return true;
     }
     
-    public static int[] indexTerms(String equation,String[] terms){
+    public static int[] indexTerms(String equation,String[] terms, int[] coefficients){
+    	for(int i = 1;i<equation.length();++i){
+    		if((equation.substring(i-1,i)).equals("x^2")){
+    			coefficients[0] = Integer.parseInt(equation.substring(0,i));
+    		}
+    		else coefficients[0] = 0;
+    	}
     	
+    	for(int i = 1;i<equation.length();++i){
+    		if((equation.substring(i-1,i)).equals("y^2")){
+    			coefficients[2] = Integer.parseInt(equation.substring(0,i));
+    		}
+    	}
+    	return coefficients;
     }
     
     public static void assignCoeff(String equation){
@@ -51,7 +58,6 @@ public class Conics {
         equation = br.readLine();
         equation = equation.toLowerCase();
         System.out.println("you entered: " + equation); //test
-        if(isCoeff(equation, x2)) System.out.println("the " + x2 + " coefficient is " + findCoeff(equation, x2)); //test
         
         //for exceptions
         try{
