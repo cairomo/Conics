6271372aa903898cdf8a1b2772bf17f9a34dba60
Chloe Mo <chloe.mo@gmail.com>
2014-02-10 11:01:31 -0800

    Create Main.java
    creates window

---
+++ Main.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		final int DISPLAY_WIDTH = 800;
+
+		final int DISPLAY_HEIGHT = 700;
+
+		JFrame f = new JFrame("Conics");
+
+		f.setSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+		Display display = new Display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+		f.setLayout(null);
+
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		f.setTitle("Conics");
+
+		f.add(display);
+
+		f.setVisible(true);
+	}
+}
